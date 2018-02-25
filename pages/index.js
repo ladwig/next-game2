7e420c81a6a4ec7b714e135bad2fbf972cdb4f0c
Daniel Ladwig <daniel.ladwig@hotmail.de>
2018-02-25 09:02:07 +0100

    Made the basics work

--- pages/index.js
+++ pages/index.js
@@ -4,41 +4,56 @@ class Index extends React.Component {
   constructor(props) {
     super(props)
 
+    const width = 508
+    const height = 500
+    const playerSize = 32
+    const enemySize = 25
+
     this.state = {
-      x: 20,
-      y: 20,
-      nx: 2,
-      ny: 2,
-      playerSize: 32
+      width: width,
+      height: height,
+      x: 0,
+      y: 0,
+      ex: 0,
+      ey: 0,
+      playerSpeed: 20,
+      playerSize: playerSize,
+      enemySize: enemySize
     }
   }
 
   componentWillMount() {
-  if (!global.window) {
-    return
-  }
-
-  document.body.addEventListener('keydown', this.onKeyDown)
+    if (!global.window) {
+      return
+    }
+    document.body.addEventListener('keydown', this.onKeyDown)
   }
 
   componentWillUnmount() {
-  if (!global.window) {
-    return
-  }
-
-  document.body.removeEventListener('keydown', this.onKeyDown)
+    if (!global.window) {
+      return
+    }
+    document.body.removeEventListener('keydown', this.onKeyDown)
   }
 
   componentDidMount() {
+    //Player spawn position
     this.setState({
       x: this.canvas.width / 2,
-      y: this.canvas.height - 30
+      y: this.canvas.height -35,
+      ex: (this.canvas.width / 2) + 30,
+      ey: this.canvas.height - 55
     })
-
-    setInterval(this.draw, 10)
   }
 
-  draw = () => {
+  componentDidUpdate() {
+    this.drawPlayer()
+    this.drawEnemy()
+    console.log(this.state)
+  }
+
+//Draw function for the player
+  drawPlayer = () => {
     const context = this.canvas.getContext('2d')
 
     context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -50,32 +65,93 @@ class Index extends React.Component {
     context.closePath();
   }
 
+//Draw function for the enemies
+  drawEnemy = () => {
+    const context = this.canvas.getContext('2d')
+
+
+
+    context.beginPath();
+    context.rect(this.state.ex, this.state.ey, this.state.enemySize, this.state.enemySize);
+    context.fillStyle = '#fff';
+    context.fill();
+    context.closePath();
+
+  }
+
+//Player walking
+
   onKeyDown = event => {
-    switch (event.keyCode) {
-      case 39:
-        this.setState({
-          x: this.state.x + 20
-        })
-        break
-      case 37:
-        this.setState({
-          x: this.state.x - 20
-        })
-        break
-    default:
-      return
+    const { x, y, playerSpeed, playerSize, width, height } = this.state
+
+    if (this.state.x !== 0 || this.state.y !== 0) {
+      switch (event.keyCode) {
+        case 39:
+        case 68:
+          if ((x + playerSize) > (width - playerSize)) {
+            this.setState({
+              x: this.state.x + (width - ( x + playerSize))
+            })
+            return
+          }
+          this.setState({
+            x: x + playerSpeed
+          })
+          break
+        case 37:
+        case 65:
+          if ((x - playerSize) < 0) {
+            this.setState({
+              x: 0
+            })
+            return
+          }
+          this.setState({
+            x: x - playerSpeed
+          })
+          break
+        case 40:
+        case 83:
+          if ((y + playerSize) > (height - playerSize)) {
+            this.setState({
+              y: this.state.y + (height - ( y + playerSize))
+            })
+            return
+          }
+          this.setState({
+            y: y + playerSpeed
+          })
+          break
+        case 38:
+        case 87:
+          if (false) {
+            this.setState({
+              y: 0
+            })
+            return
+          }
+          this.setState({
+            y: y - playerSpeed
+          })
+          break
+
+      default:
+        return
+      }
     }
-        event.preventDefault()
+
+    event.preventDefault()
   }
 
   render () {
    return (
      <div>
-      <canvas width="500" height="500" ref={element => this.canvas = element}/>
+      <canvas width={this.state.width} height={this.state.height} ref={element => this.canvas = element}/>
 
       <style jsx>{`
         canvas {
           background: black;
+          border: 0;
         }
       `}</style>
 
