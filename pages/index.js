8fe3a22da873fa09142d9556705772b5d6c65da8
Daniel Ladwig <daniel.ladwig@hotmail.de>
2018-02-26 08:03:52 +0100

    Build a simple collision dectection

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,26 @@ class Index extends React.Component {
 
     const width = 508
     const height = 500
-    const playerSize = 32
+    const playerSize = 25
     const enemySize = 25
 
     this.state = {
+
+      //Game settings
       width: width,
       height: height,
+
+      //Player settings
       x: 0,
       y: 0,
-      ex: 0,
-      ey: 0,
       playerSpeed: 20,
       playerSize: playerSize,
-      enemySize: enemySize
+
+      //Enemy settings
+      enemySpeed: 0.5,
+      enemySize: enemySize,
+      enemyMax: 10,
+      enemies: []
     }
   }
 
@@ -41,106 +48,161 @@ class Index extends React.Component {
     this.setState({
       x: this.canvas.width / 2,
       y: this.canvas.height -35,
-      ex: (this.canvas.width / 2) + 30,
-      ey: this.canvas.height - 55
     })
+
+    setInterval(this.draw, 10)
+    const {enemySize, enemySpeed, enemyMax, enemies} = this.state
+    let counter = 0
+
+    while(counter < enemyMax) {
+      enemies.push({
+        ex: Math.floor((Math.random() * (this.state.width-enemySize)) + 1),
+        ey: Math.floor((Math.random() * (this.state.height-enemySize)) + 1)
+      })
+      counter++
+    }
+
   }
 
   componentDidUpdate() {
-    this.drawPlayer()
-    this.drawEnemy()
-    console.log(this.state)
+    this.draw()
+  }
+
+  //Draw everything
+  draw = () => {
+    this.drawPlayer();
+
+    let i;
+    for(i = 0; i < 10; i++) {
+    this.drawEnemy(this.state.enemies[i].ex, this.state.enemies[i].ey)
+    this.collision(this.state.x, this.state.y, this.state.enemies[i].ex, this.state.enemies[i].ey)
+    }
   }
 
 //Draw function for the player
   drawPlayer = () => {
-    const context = this.canvas.getContext('2d')
+    const context = this.canvas.getContext('2d');
 
     context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     context.beginPath();
     context.rect(this.state.x, this.state.y, this.state.playerSize, this.state.playerSize);
-    context.fillStyle = 'red';
+    context.fillStyle = '#fff';
+    context.fill();
+    context.closePath();
+}
+
+
+
+//Draw function for the enemies
+  drawEnemy = (ex, ey) => {
+    const {enemySize, enemySpeed} = this.state
+    const context = this.canvas.getContext('2d')
+    context.beginPath();
+    context.rect(ex, ey, enemySize, enemySize);
+    context.fillStyle = '#ffe066';
     context.fill();
     context.closePath();
   }
 
-//Draw function for the enemies
-  drawEnemy = () => {
-    const context = this.canvas.getContext('2d')
+//Collision
+  collision = (x, y, ex, ey) => {
+    if (this.state.collided) {
+      return
+    }
 
-
-
-    context.beginPath();
-    context.rect(this.state.ex, this.state.ey, this.state.enemySize, this.state.enemySize);
-    context.fillStyle = '#fff';
-    context.fill();
-    context.closePath();
+    if (x < ex + this.state.enemySize && x + this.state.playerSize  > ex &&
+		    y < ey + this.state.enemySize && y + this.state.playerSize > ey) {
+        this.setState({
+          collided: true
+        })
+    }
 
   }
 
 //Player walking
 
   onKeyDown = event => {
-    const { x, y, playerSpeed, playerSize, width, height } = this.state
 
     if (this.state.x !== 0 || this.state.y !== 0) {
       switch (event.keyCode) {
         case 39:
         case 68:
-          if ((x + playerSize) > (width - playerSize)) {
-            this.setState({
-              x: this.state.x + (width - ( x + playerSize))
-            })
-            return
-          }
-          this.setState({
-            x: x + playerSpeed
-          })
+          this.movePlayerR()
           break
         case 37:
         case 65:
-          if ((x - playerSize) < 0) {
-            this.setState({
-              x: 0
-            })
-            return
-          }
-          this.setState({
-            x: x - playerSpeed
-          })
+          this.movePlayerL()
           break
         case 40:
         case 83:
-          if ((y + playerSize) > (height - playerSize)) {
-            this.setState({
-              y: this.state.y + (height - ( y + playerSize))
-            })
-            return
-          }
-          this.setState({
-            y: y + playerSpeed
-          })
+          this.movePlayerU()
           break
         case 38:
         case 87:
-          if (false) {
-            this.setState({
-              y: 0
-            })
-            return
-          }
-          this.setState({
-            y: y - playerSpeed
-          })
+          this.movePlayerD()
           break
-
       default:
         return
       }
     }
-
     event.preventDefault()
+  }
+
+  //Move player right
+  movePlayerR = () => {
+    const { x, y, playerSpeed, playerSize, width, height } = this.state
+    if ((x + playerSize) > (width - playerSize)) {
+      this.setState({
+        x: this.state.x + (width - ( x + playerSize))
+      })
+      return
+    }
+    this.setState({
+      x: x + playerSpeed
+    })
+  }
+
+  //Move player left
+  movePlayerL = () => {
+    const { x, y, playerSpeed, playerSize, width, height } = this.state
+    if ((x - playerSize) < 0) {
+      this.setState({
+        x: 0
+      })
+      return
+    }
+    this.setState({
+      x: x - playerSpeed
+    })
+  }
+
+  //Move player up
+  movePlayerU = () => {
+    const { x, y, playerSpeed, playerSize, width, height } = this.state
+    if ((y + playerSize) > (height - playerSize)) {
+      this.setState({
+        y: this.state.y + (height - ( y + playerSize))
+      })
+      return
+    }
+    this.setState({
+      y: y + playerSpeed
+    })
+  }
+
+  //Move player down
+  movePlayerD = () => {
+    const { x, y, playerSpeed, playerSize, width, height } = this.state
+    if (y < playerSize) {
+      this.setState({
+        y: y - y
+      })
+      return
+    }
+    this.setState({
+      y: y - playerSpeed
+    })
   }
 
   render () {
